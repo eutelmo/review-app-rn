c4040fa5b82aec192230af84dc1297ec8925c80b
Telmo J Moura <telmop1999@gmail.com>
2023-03-09 14:53:48 +0000

    global Styles

--- App.js
+++ App.js
@@ -1,6 +1,42 @@
-import { StatusBar } from "expo-status-bar";
+import { useState, useEffect, useCallback } from "react";
+
 import Home from "./screens/home";
+import * as Font from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
+// const getFonts = () =>
+//   Font.loadAsync({
+//     "nunito-regular": require("./assets/fonts/NunitoSans-Regular.ttf"),
+//     "nunito-bold": require("./assets/fonts/NunitoSans-Bold.ttf"),
+//   });
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await Font.loadAsync({
+          "nunito-regular": require("./assets/fonts/NunitoSans-Regular.ttf"),
+          "nunito-bold": require("./assets/fonts/NunitoSans-Bold.ttf"),
+        });
+        // await new Promise((resolve) => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setFontsLoaded(true);
+        await SplashScreen.hideAsync();
+      }
+    }
+
+    prepare();
+  }, []);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return <Home />;
 }
--- screens/reviews.js
+++ screens/reviews.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
+import { globalStyles } from "../styles/global";
 
 
 export default function Review(){
   return( 
-    <View style={styles.container}>
+    <View style={globalStyles.container}>
       <Text>Review Screen</Text>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    padding: 24,
-  }
+ 
 })
