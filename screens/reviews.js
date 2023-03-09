de7f6cacc126241410bb9c3b88fb46b037d1287b
Telmo J Moura <telmop1999@gmail.com>
2023-03-09 12:02:27 +0000

    create screens

--- screens/reviews.js
+++ screens/reviews.js
@@ -1,1 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+
+export default function Review(){
+  return( 
+    <View style={styles.container}>
+      <Text>Review Screen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 24,
+  }
+})
